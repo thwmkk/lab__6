f04e5ad3263c7124f4b1625504ae366ae9966331
sasukeluvbot@gmail.com <sasukeluvbot@gmail.com>
2024-06-11 02:20:03 +0800

    удалила методы UpdateState и Render

--- Emitter.cs
+++ Emitter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,25 @@ namespace WindowsFormsApp1
 {
     internal class Emitter
     {
+        List<Particle> particles = new List<Particle>();
+        public int MousePositionX;
+        public int MousePositionY;
+
+        public void UpdateState()
+        {
+            /* АНТИ-КОПИПАСТА, устал я код копипасть,
+               так что давайте сами копируйте из своего кода,
+               то что было на форме в методе UpdateState :Р */
+        }
+
+        public void Render(Graphics g)
+        {
+            // ну тут так и быть уж сам впишу...
+            // это то же самое что на форме в методе Render
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+        }
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -22,77 +22,7 @@ namespace WindowsFormsApp1
 
             }
 
-        private void UpdateState()
-        {
-            foreach (var particle in particles)
-            {
-                particle.Life -= 1;  // не трогаем
-                if (particle.Life < 0)
-                {
-                    // тоже не трогаем
-                    particle.Life = 20 + Particle.rand.Next(100);
-                    particle.X = MousePositionX;
-                    particle.Y = MousePositionY;
-
-                    /* это убираем
-                    particle.Direction = Particle.rand.Next(360);
-                    particle.Speed = 1 + Particle.rand.Next(10);
-                    */
-
-                    /* ЭТО ДОБАВЛЯЮ, тут сброс состояния частицы */
-                    var direction = (double)Particle.rand.Next(360);
-                    var speed = 1 + Particle.rand.Next(10);
-
-                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
-                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
-                    /* конец ЭТО ДОБАВЛЯЮ  */
-
-                    // это не трогаем
-                    particle.Radius = 2 + Particle.rand.Next(10);
-                }
-                else
-                {
-                    /* это все убираем, тут у нас старый пересчет положения частицы в пространстве 
-                    var directionInRadians = particle.Direction / 180 * Math.PI;
-                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
-                    particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
-                    */
-
-                    // и добавляем новый, собственно он даже проще становится, 
-                    // так как теперь мы храним вектор скорости в явном виде и его не надо пересчитывать
-                    particle.X += particle.SpeedX;
-                    particle.Y += particle.SpeedY;
-                }
-            }
-            for (var i = 0; i < 10; ++i)
-            {
-                if (particles.Count < 500)
-                {
-                    // а у тут уже наш новый класс используем
-                    var particle = new ParticleColorful();
-                    // ну и цвета меняем
-                    particle.FromColor = Color.Yellow;
-                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
-                    particle.X = MousePositionX;
-                    particle.Y = MousePositionY;
-                    particles.Add(particle);
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
-
-        // функция рендеринга
-        private void Render(Graphics g)
-        {
-            // утащили сюда отрисовку частиц
-            foreach (var particle in particles)
-            {
-                particle.Draw(g);
-            }
-        }
+      
 
         // ну и обработка тика таймера, тут просто декомпозицию выполнили
         private void timer1_Tick(object sender, EventArgs e)
