ac3c039cfe58325a9c969577806babf3a8b7f3c7
sasukeluvbot@gmail.com <sasukeluvbot@gmail.com>
2024-06-11 02:37:15 +0800

    добавила классы GravityPoint и AntiGravityPoint

--- Particle.cs
+++ Particle.cs
@@ -107,5 +107,35 @@ namespace WindowsFormsApp1
                     );
             }
         }
+        public class GravityPoint : IImpactPoint
+        {
+            public int Power = 100; // сила притяжения
+
+            // а сюда по сути скопировали с минимальными правками то что было в UpdateState
+            public override void ImpactParticle(Particle particle)
+            {
+                float gX = X - particle.X;
+                float gY = Y - particle.Y;
+                float r2 = (float)Math.Max(100, gX * gX + gY * gY);
+
+                particle.SpeedX += gX * Power / r2;
+                particle.SpeedY += gY * Power / r2;
+            }
+        }
+        public class AntiGravityPoint : IImpactPoint
+        {
+            public int Power = 100; // сила отторжения
+
+            // а сюда по сути скопировали с минимальными правками то что было в UpdateState
+            public override void ImpactParticle(Particle particle)
+            {
+                float gX = X - particle.X;
+                float gY = Y - particle.Y;
+                float r2 = (float)Math.Max(100, gX * gX + gY * gY);
+
+                particle.SpeedX -= gX * Power / r2; // тут минусики вместо плюсов
+                particle.SpeedY -= gY * Power / r2; // и тут
+            }
+        }
     }
 }
